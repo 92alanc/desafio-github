78ac6b91df08dcca271a762a70329ff430bcc7f1
Alan Camargo <alan.uk.br@gmail.com>
2023-05-19 17:28:31 -0300

    Implement user summary adapter and view holder

---
+++ feature-users/src/main/kotlin/com/alancamargo/desafiogithub/users/ui/adapter/usersummary/UserSummaryViewHolder.kt
@@ -0,0 +1,14 @@
+package com.alancamargo.desafiogithub.users.ui.adapter.usersummary
+
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.alancamargo.desafiogithub.domain.usersummary.model.UserSummary
+import com.alancamargo.desafiogithub.users.databinding.ItemUserSummaryBinding
+
+internal class UserSummaryViewHolder(
+    private val binding: ItemUserSummaryBinding
+) : ViewHolder(binding.root) {
+
+    fun bindTo(userSummary: UserSummary) {
+        // TODO
+    }
+}
