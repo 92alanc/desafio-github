21af83e2d73d849e8ecda7d34c30cc11de7d6648
Alan Camargo <alan.uk.br@gmail.com>
2023-05-19 17:22:56 -0300

    Implement repository adapter and view holder

---
+++ feature-users/src/main/kotlin/com/alancamargo/desafiogithub/users/ui/adapter/repository/RepositoryViewHolder.kt
@@ -0,0 +1,14 @@
+package com.alancamargo.desafiogithub.users.ui.adapter.repository
+
+import androidx.recyclerview.widget.RecyclerView
+import com.alancamargo.desafiogithub.domain.repository.model.Repository
+import com.alancamargo.desafiogithub.users.databinding.ItemRepositoryBinding
+
+internal class RepositoryViewHolder(
+    private val binding: ItemRepositoryBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bindTo(repository: Repository) {
+        // TODO
+    }
+}
